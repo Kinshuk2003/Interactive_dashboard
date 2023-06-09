89dcb7e4924111e328a129dd6cebf3b8ad07e04d
Kinshuk Goyal <72309507+Kinshuk2003@users.noreply.github.com>
2023-06-09 09:46:04 +0530

    app files

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BsCurrencyDollar} from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { Stacked,Pie, Button, SparkLine } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartDat } from '../data/dummy';
+
+
+
+
+
+const Ecommerce = () => {
+  return (
+    <div>Ecommerce</div>
+  )
+}
+
+export default Ecommerce
